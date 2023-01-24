e8faa72cfff90ea174a6bb48d52f6f4c632a23ac
sajjad <sajjad@yahoo.com>
2023-01-23 22:29:21 -0500

    first commit

---
+++ src/Abyte.java
@@ -0,0 +1,26 @@
+
+
+public class Abyte {
+
+	public static void main(String[] args) {
+		add() ;
+		add() ;
+		
+		
+		
+	
+		
+
+	}
+public static void add(){
+	byte x ;
+	byte y ;
+	byte z ;
+	x = 3 ;
+	y = 2 ;
+	z = (byte)(x+y) ;
+	
+	System.out.println(z);
+	
+}
+}
